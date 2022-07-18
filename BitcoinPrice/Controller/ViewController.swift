2c3a5ac5642dadfc376c3850d2d96399cf749339
Alicja Gruca <alicjagruca6@gmail.com>
2022-07-19 01:13:17 +0200

    :sparkles: Adding functionality of PickerView

--- BitcoinPrice/Controller/ViewController.swift
+++ BitcoinPrice/Controller/ViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    let coinManager = CoinManager()
+    
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
+    }
+    
+    
+    //MARK: - UIPickerViewDataSource
+    
+    //Called by the picker view when it needs the number of components.
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    //Called by the picker view when it needs the number of rows for a specified component.
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    
+    
+    //MARK: - UIPickerViewDelegate
+    
+    //Called by the picker view when it needs the title to use for a given row in a given component.
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
     }
 
+    // This method is triggered whenever the user makes a change to the picker selection.
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        coinManager.getBitcoinPrice(for: coinManager.currencyArray[row])
+    }
 
 }
 
---
+++ BitcoinPrice/Model/CoinManager.swift
@@ -0,0 +1,20 @@
+//
+//  CoinManager.swift
+//  BitcoinPrice
+//
+//  Created by Alicja Gruca on 18/07/2022.
+//
+
+import Foundation
+
+struct CoinManager {
+    
+    let bitcoinPricesURL: String = ""
+    let apiKey: String = ""
+    
+    let currencyArray: [String] = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    func getBitcoinPrice(for currency: String) {
+        
+    }
+}
